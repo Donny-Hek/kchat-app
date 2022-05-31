54556f89087d0e17bb0ba6102175f1f62cb6882d
Donny-Hek <69718795+Donny-Hek@users.noreply.github.com>
2022-05-31 17:05:25 +0300

    чистка

--- application/models/model_home.php
+++ application/models/model_home.php
@@ -62,8 +62,6 @@ class Model_Home extends Model
     function logout_user()
     {
         unset($_POST);
-//        unset($_SESSION['login']);
-//        unset($_SESSION['id']);
         session_destroy();
         header("location: http://localhost/kchat/");
     }
